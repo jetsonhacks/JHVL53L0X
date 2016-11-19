127acd4a1b20a91136cbc72e085424b47b47146d
jetsonhacks <jim@jetsonhacks.com>
2016-11-19 12:21:21 -0800

    Reload udev rules

--- installPre.sh
+++ installPre.sh
@@ -3,3 +3,6 @@
 sudo apt-get install libi2c-dev i2c-tools -y
 # Setup permissions to run i2c in user space
 sudo bash -c 'echo KERNEL==\"i2c-[0-7]\",MODE=\"0666\" > /etc/udev/rules.d/90-i2c.rules'
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
